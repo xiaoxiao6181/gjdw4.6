8b635956d893b910f79b03b34cb97744755d84f5
xiaoxiao6181 <167971162+xiaoxiao6181@users.noreply.github.com>
2026-04-07 01:16:52 +0800

    Add files via upload

---
+++ assets/useMoney-BGTDnpC9.js
@@ -0,0 +1,1 @@
+import{l as t,r as s,m as n}from"./index-K_DaNyIo.js";const u=t("money",()=>{const e=s();return{money:n({get:()=>e.value,set:o=>{e.value=o}}),_money:e}},{persist:!0});export{u};
---
+++ assets/useProduct-gOA0B4U2.js
@@ -0,0 +1,1 @@
+import{l as a,r,m as s}from"./index-K_DaNyIo.js";const d=a("product",()=>{const t=r();return{data:s({get:()=>t.value,set:e=>{t.value=e}}),_data:t}},{persist:!0});export{d as u};
