baafdfa9a7dd5a26ea4d0c3a16e8c76938fd6fb8
xiaoxiao6181 <167971162+xiaoxiao6181@users.noreply.github.com>
2026-04-07 21:42:06 +0800

    Add files via upload

---
+++ assets/useAuth-gM60UNXF.js
@@ -0,0 +1,1 @@
+import{l as u,r as s,m as a}from"./index-CD2ZyB4C.js";const h=u("auth",()=>{const t=s();return{auth:a({get:()=>t.value,set:e=>{t.value=e}}),_auth:t}},{persist:!0});export{h as u};
---
+++ assets/useConf-BVxrfVIQ.js
@@ -0,0 +1,1 @@
+import{l as a,r as s,m as o}from"./index-CD2ZyB4C.js";const u=a("conf",()=>{const t=s();return{data:o({get:()=>t.value,set:e=>{t.value=e}}),_data:t}},{persist:!0});export{u};
